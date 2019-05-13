2f864b61d413f860d631924c67ced310bca65e6f
Srinath <31591297+Srinath-V-S@users.noreply.github.com>
2019-05-13 22:35:03 +0530

    Using FeedParser
    This Notfication Script uses feedparser and notify2 library.

---
+++ desktop_notifier.py
@@ -0,0 +1,25 @@
+import notify2
+import feedparser
+
+import os
+import time
+
+
+def parse_feed():
+	f = feedparser.parse("http://feeds.bbci.co.uk/news/rss.xml") #returns parsed data in form of dictionary
+	ICON_PATH = os.getcwd() + '/news_icon.ico' # set icon for the notification
+	notify2.init('Desktop News Notifier') # initializing dbus connection
+	# Loop through parsed data to get relevant info for notification
+	for news_item in f['items']:
+		n = notify2.Notification(news_item['title'],news_item['summary'],icon = ICON_PATH)
+
+	n.set_urgency(notify2.URGENCY_NORMAL)
+	n.show()
+	n.set_timeout(200000) #notification stays for 15 seconds
+	time.sleep(100)     #time difference between each notification
+
+
+
+
+if __name__ == '__main__':
+	parse_feed()
